b13d5ed48e64d257c24555aec322ddb6e0fbdd57
mohas95 <moha.debbagh95@gmail.com>
2026-07-17 11:30:53 -0400

    updata data handling library to latest repo and add data handling for platform controls

--- plugins/platform_control.py
+++ plugins/platform_control.py
@@ -73,6 +73,8 @@ def register_control_scheduler_sockets(ControlScheduler, socketio, app):
     global control_schedule_list
     global host_url 
     global control_scheduler_alias
+    global data_handler, active_experiment, last_seen_data
+
 
     control_scheduler_alias = ControlScheduler
 
@@ -93,22 +95,89 @@ def register_control_scheduler_sockets(ControlScheduler, socketio, app):
                                             )
             control_device.start()
             control_schedule_list[control_device.device_name]= control_device
+    
+    experiment_config_file = load_config(app.root_path, "panels.json")[experiment_panel_association]['config']['set_to']
+    experiment_config = load_config(app.root_path, experiment_config_file)
+    active_experiment = experiment_config["active_experiment"]["set_to"]
 
-                    
+    if experiment_config["data_logging"]["set_to"]:
+        data_handler = dh.SQLiteDataHandler(experiment_config["database_path"]["set_to"],dh.SCHEDULED_EVENTS_CONT_TABLE)
+        data_handler.start(continuous_platform_control_logging,routine_name="continuous_platform_control_logging")
+
+    for device_id, control_device in control_schedule_list.items():
+        #register dataoutput callback to capture_registry
+        capture_registry.register(f"{device_id}_data", lambda: capture_platform_control_data(device_id))
+
+        #get the last output
+        data =control_device.get_last_output()
+        last_seen_data[device_id] = data                
 
 # def reload_routine(SerialReader, ControlScheduler, socketio, app):
 def reload_routine(socketio, app):
     global control_schedule_list, control_scheduler_alias
+    global data_handler
 
     # Kill control schedulers
-    for device in control_schedule_list.values():
+    for device_id, device in control_schedule_list.items():
         device.kill()
+        #deregister capture register callbacks
+        capture_registry.deregister(f"{device_id}_data")
     control_schedule_list.clear()
+
+    # Kill datahandlers
+    if data_handler:
+        data_handler.kill_all()
+        data_handler = None
+
 
     # Re-register
     register_control_scheduler_sockets(control_scheduler_alias, socketio, app)
 
 
+def continuous_platform_control_logging():
+    global control_schedule_list, data_handler, last_seen_data, active_experiment
+
+    # output_data = {}
+
+    for device_id, control_device in control_schedule_list.items():
+        data_out =control_device.get_last_output()
+
+        # output_data[device_name] = data
+
+
+        if data_out != last_seen_data[device_id]:
+            last_seen_data[device_id] = data_out
+            # print(data_out)
+
+            sorted_data= {
+                "experiment_id": active_experiment,
+                "device_id": device_id,
+                "event_type": device_id,
+                "status": data_out.get("data"),
+                "schedule_json": json.dumps(data_out.get("schedule")),
+                "timestamp": data_out.get("timestamp_utc")        
+                }
+
+            data_handler.insert("scheduled_events_continuous",sorted_data)
+
+def capture_platform_control_data(device_id):
+    global control_schedule_list, data_handler, last_seen_data, active_experiment
+
+    control_device = control_schedule_list[device_id]
+    data_out = control_device.get_last_output()
+
+
+    sorted_data= {
+                "data_table": "scheduled_events",
+                "experiment_id": active_experiment,
+                "device_id": device_id,
+                "event_type": device_id,
+                "status": data_out.get("data"),
+                "schedule_json": json.dumps(data_out.get("schedule")),
+                "timestamp": data_out.get("timestamp_utc")         
+                }
+        
+    return sorted_data
 
 # API
 @plugin_blueprint.route("/processed_data")
